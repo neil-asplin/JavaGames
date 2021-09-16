0c4e01b1902d17dcc8e875163b1ec4998615937e
Neil Asplin <neilasplin@gmail.com>
2021-09-16 14:26:36 +1200

    star 2 example added using utility timer

---
+++ src/com/neilasplin/javagames/utiltimer/UtilityTimerEx.java
@@ -0,0 +1,29 @@
+package com.neilasplin.javagames.utiltimer;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class UtilityTimerEx extends JFrame {
+
+    public UtilityTimerEx(){
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Star 2");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new UtilityTimerEx();
+            ex.setVisible(true);
+        });
+    }
+}
